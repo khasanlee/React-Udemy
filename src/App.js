ef52b5ff6a75169095e3073ee2114949bcbb6d9d
khasanlee <xasaniskandarov2000@gmail.com>
2025-03-26 01:52:45 +0900

    learning props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import GoalList from './components/GoalList';
 function App() {
+  const courseGoals = [
+    { id: 'cg1', text: 'Finish the course' },
+    { id: 'cg2', text: 'Learn all about the course main topic' },
+    { id: 'cg3', text: 'Help other students in the course Q&A' }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
